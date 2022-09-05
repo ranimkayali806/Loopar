f3dbdbb95a75efa052d760a58804d86acb4c4c79
Ranim Kayali <eng.ranimkayali@gmail.com>
2022-09-05 13:54:45 +0200

    first commit

---
+++ övningwhile.py
@@ -0,0 +1,68 @@
+# Loop 1
+
+#for x in range(0,11):  #or range(11)
+# print(x)
+
+# start = 0 
+# slut = 11
+# aktuelltal = start
+# while aktuelltal < slut:
+#     print(aktuelltal)
+#     aktuelltal = aktuelltal + 1
+#.............................
+
+    # Loop 2
+# num1 = int(input("mata in numb1:"))
+# num2 = int(input("mata in munb2:"))
+#if num1 > num2:
+# for x in range(num1,num2,-1):
+#      print(x)
+ #elif num1 < num2:
+#for x in range(num1,num2):
+ #       print(x)
+#.............................
+# while
+# num1 = int(input("mata in numb1:"))
+# num2 = int(input("mata in numb2:"))
+# x = num1
+# while x < num2:
+#     print(x)
+#     x = x + 1
+#...................................
+# Loop 3
+# while True:   #not correct
+#  num1 = int(input("mata in numb1:"))
+#  num2 = int(input("mata in numb2:"))
+#  num = num1 + num2
+#  print("num is:" , num)
+#  r = False
+#  frå = input("vill du förtsätta j/n?")
+#  if frå == "j":
+#    print("r == True")
+# else:
+#     print("programmet avbryts") 
+
+
+
+    #,...................
+    # Loop 4
+n = 1
+while n <= 10: 
+     tal = int(input("mata in tal1:"))
+     x = tal
+     x1 = x + tal
+     n = n + 1
+print(x1)
+    #........................
+
+    #Loop 5                      #correct
+# tal = int(input("mata in tal1:"))
+# for x  in range(1,tal):
+#     print(x)
+
+    #............................
+
+    #Loop6
+
+
+
